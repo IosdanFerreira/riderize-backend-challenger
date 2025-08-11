0284966480f8a5ed7f1d6309e4b9ea00452fbb36
Iosdan Ferreira <iosdan.ferreira@gmail.com>
2025-08-11 13:05:12 -0300

    upgrade build

--- src/shared/redis/redis.client.ts
+++ src/shared/redis/redis.client.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
 export const redisClient = new Redis({
-  host: process.env.REDIS_HOST || "redis",
-  port: Number(process.env.REDIS_PORT) || 6379,
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
 });
